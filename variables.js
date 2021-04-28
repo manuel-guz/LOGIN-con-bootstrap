3f955bc08d43a6f7374538ddea4e83f61803f13f
Manuel Guzman <ale_cba17@hotmail.com>
2021-04-27 21:57:47 -0300

    Primer documento JS

---
+++ variables.js
@@ -0,0 +1,29 @@
+
+
+var nombre = "berni";
+
+console.log(nombre);  // escribir por consola
+
+var numero=1000;
+console.log(numero);
+//variables tipo objetos
+
+var persona = {
+   nombre: "nico",
+   apellido:"Fernandez",
+   telefoono: 123234345
+
+};
+
+console.log(persona);
+
+//trabajando con el DOM
+
+
+let titulo = document.getElementById('titulo');
+
+titulo.innerHTML = "hola mundooo por JS";
+
+let parrafo = document.getElementById('parrafo');
+
+parrafo.innerHTML = "este es el parrafo por JS";
